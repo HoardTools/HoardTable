65691b1d497fea0b278235e665c496fa51b2d6de
Dax Harris <matteovh@gmail.com>
2023-04-29 12:00:33 -0400

    Mantine UI setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ReactNode } from "react";
+import { MantineProvider } from "@mantine/core";
+import { Notifications } from "@mantine/notifications";
+import { ModalsProvider } from "@mantine/modals";
+
+import "./app.scss";
+
+function MantineSetup(props: {
+    children?: ReactNode | ReactNode[];
+}): JSX.Element {
+    return (
+        <MantineProvider
+            theme={{
+                colors: {
+                    brand: [
+                        "#fff8da",
+                        "#ffebad",
+                        "#ffdd7d",
+                        "#ffcf4b",
+                        "#ffc21a",
+                        "#e6a800",
+                        "#b38300",
+                        "#805d00",
+                        "#4e3800",
+                        "#1d1300",
+                    ],
+                },
+                primaryColor: "brand",
+                colorScheme: "dark",
+            }}
+            withCSSVariables
+            withGlobalStyles
+            withNormalizeCSS
+        >
+            <ModalsProvider>
+                <Notifications />
+                {props.children}
+            </ModalsProvider>
+        </MantineProvider>
+    );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="app-container">
+            <MantineSetup></MantineSetup>
+        </div>
+    );
 }
 
 export default App;
